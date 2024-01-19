544454a9cd86906463a0d6594e1825a0ec225fa4
4rcan31 <da.esfra12@gmail.com>
2024-01-19 16:59:28 -0600

    update to public method normalizeString

--- src/main/java/decoder/core/DecoderMultipleOrders.java
+++ src/main/java/decoder/core/DecoderMultipleOrders.java
@@ -5,30 +5,67 @@ import java.util.List;
 
 public class DecoderMultipleOrders {
 
+    private List<Decoder> orders;
+    private String ordersString;
+    private String headerStartOrder;
+    private List<String> requiredKeys;
 
-    List<Decoder> orders;
-    String ordersString;
-    String headerStartOrder;
-
+    private List<String> errors;
 
     public DecoderMultipleOrders(String orders) {
         this.headerStartOrder = "Confirmando los datos de su pedido";
         this.ordersString = orders;
         this.orders = new ArrayList<>();
-        this.decodeOrders();
+        this.errors = new ArrayList<>();
+        this.requiredKeys = new ArrayList<>();
+       // this.decodeOrders();
     }
 
-    public String getHeaderStartOrder(){
+    public String getHeaderStartOrder() {
         return this.headerStartOrder;
     }
 
-    private void decodeOrders() {
-        String[] orderBlocks = ordersString.split(headerStartOrder);
+    public void setHeaderStarOrder(String headerStartOrder) {
+        this.headerStartOrder = headerStartOrder;
+    }
 
+    public List<String> getRequiredKeys() {
+        return requiredKeys;
+    }
+
+    public void setRequiredKeys(List<String> newRequiredKeys) {
+        requiredKeys = newRequiredKeys;
+    }
+
+    public void addRequiredKey(String key) {
+        requiredKeys.add(key);
+    }
+
+    public List<String> getErrors() {
+        return this.errors;
+    }
+
+    public boolean existError(){
+        return this.errors.size() != 0;
+    }
+
+    public void decode() {
+        String[] orderBlocks = ordersString.split(headerStartOrder);
         for (String block : orderBlocks) {
             if (!block.trim().isEmpty()) {
                 Decoder decoder = new Decoder(block);
+                this.checkRequiredKeys(decoder, requiredKeys);
                 orders.add(decoder);
+            }
+        }
+    }
+
+    private void checkRequiredKeys(Decoder decoder, List<String> requiredKeys) {
+        for (String key : requiredKeys) {
+            if(decoder.getValue(
+                decoder.normalizeString(key)
+            ) == ""){
+                this.errors.add("Falta el campo "+key+" revise su ortografia y continue");
             }
         }
     }
