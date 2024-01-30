64a01607332460ce4792951bbb70467b96d8e992
4rcan31 <da.esfra12@gmail.com>
2024-01-30 14:20:50 -0600

    Build the promt and set the model

--- src/main/java/Kernel/openia/PromptProcessorGpt.java
+++ src/main/java/Kernel/openia/PromptProcessorGpt.java
@@ -1,8 +1,7 @@
-package Kernel.openia; 
+package Kernel.openia;
 
 import java.text.Normalizer;
 
-import com.google.gson.Gson;
 
 import Kernel.config.Config;
 
@@ -18,48 +17,43 @@ public class PromptProcessorGpt {
         this.model = "gpt-3.5-turbo-instruct";
     }
 
-   public String buildPrompt() {
-    return this.normalizeString(new StringBuilder("Dado el siguiente texto de un pedido:")
-    .append(this.block)
-    .append("Realiza las siguientes transformaciones:")
-    .append("1. Elimina la linea que contiene Direccion exacta.")
-    .append("2. Si la linea Dirección exacta tiene información redundante, crea una nueva línea con la etiqueta Referencia: seguida ")
-    .append("de la información redundante. 3. En la línea Descripción de compra o donde estén los productos, usa la etiqueta Productos: seguida de la lista de ")
-    .append("productos en el formato dado: Productos: [productos].iterable.   - Ejemplo: COMBOS SMARTWATCH D20 NEGRO + AUDIFONOS F9-5C NEGRO.   - Asegúrate ")
-    .append("de manejar correctamente los casos donde la cantidad es 1 o no está presente.4. Si la línea de productos contiene información sobre el envío con un precio, crea una nueva línea con la etiqueta Envío: seguida del precio del envío.5. Usa la etiqueta Comentario: seguida del comentario del cliente, si está presente.6. Ignora la línea ¿Es correcto su pedido? MANDADITOS ? ya que no es un comentario relevante.")
-    .append("Genera la salida en el formato siguiente:")
-    .append("Nombre: [Nombre]")
-    .append("Teléfono: [Teléfono]")
-    .append("Ciudad: [Ciudad]")
-    .append("Departamento: [Departamento]")
-    .append("Referencia: [Referencia]")
-    .append("Productos: [Productos]")
-    .append("Total a pagar: [Total a pagar]")
-    .append("Envío: [Envío]")
-    .append("Fecha de entrega: [Fecha de entrega]")
-    .append("Comentario: [Comentario]")
-    .toString());
-}
+    public String buildPrompt() {
+        return this.normalizeString(new StringBuilder("Dado el siguiente texto de un pedido:")
+                .append(this.block)
+                .append("Realiza las siguientes transformaciones:")
+                .append("1. Elimina la linea que contiene Direccion exacta.")
+                .append("2. Si la linea Dirección exacta tiene información redundante, crea una nueva línea con la etiqueta Referencia: seguida ")
+                .append("de la información redundante. 3. En la línea Descripción de compra o donde estén los productos, usa la etiqueta Productos: seguida de la lista de ")
+                .append("productos en el formato dado: Productos: [productos].iterable.   - Ejemplo: COMBOS SMARTWATCH D20 NEGRO + AUDIFONOS F9-5C NEGRO.   - Asegúrate ")
+                .append("de manejar correctamente los casos donde la cantidad es 1 o no está presente.4. Si la línea de productos contiene información sobre el envío con un precio, crea una nueva línea con la etiqueta Envío: seguida del precio del envío.5. Usa la etiqueta Comentario: seguida del comentario del cliente, si está presente.6. Ignora la línea ¿Es correcto su pedido? MANDADITOS ? ya que no es un comentario relevante.")
+                .append("Genera la salida en el formato siguiente:")
+                .append("Nombre: [Nombre]")
+                .append("Teléfono: [Teléfono]")
+                .append("Ciudad: [Ciudad]")
+                .append("Departamento: [Departamento]")
+                .append("Referencia: [Referencia]")
+                .append("Productos: [Productos]")
+                .append("Total a pagar: [Total a pagar]")
+                .append("Envío: [Envío]")
+                .append("Fecha de entrega: [Fecha de entrega]")
+                .append("Comentario: [Comentario]")
+                .toString());
+    }
 
-    
-
-    public String getPromt(){
+    public String getPromt() {
         return this.prompt;
     }
 
     public String getResponse() {
         try {
             OpenAIClient openAIClient = new OpenAIClient(Config.getApiKeyOpenIA());
-            String model = "gpt-3.5-turbo-instruct";
-            String response = openAIClient.createCompletion(model, this.getPromt());
-
-return response;
+            String response = openAIClient.createCompletion(this.model, this.getPromt());
+            return response;
         } catch (Exception e) {
             System.out.println("Error");
             return "";
         }
     }
-
 
     public String normalizeString(String key) {
         // Normalizar y eliminar tildes
