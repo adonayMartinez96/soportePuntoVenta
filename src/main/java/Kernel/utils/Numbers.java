7b6b43a2b99da11b3b095bf9490b19a9227208a6
4rcan31 <da.esfra12@gmail.com>
2024-02-08 17:28:12 -0600

    Add lib for drive the numbers in strings

---
+++ src/main/java/Kernel/utils/Numbers.java
@@ -0,0 +1,15 @@
+package Kernel.utils;
+
+public class Numbers {
+
+    public static boolean isNumber(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
